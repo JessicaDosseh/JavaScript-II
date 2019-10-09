8c9048acc88331e33e04384e34fc4cccaa839f90
Jessica <Jessica@users-iMac.local>
2019-10-08 20:15:13 -0400

    done with main work

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -41,23 +41,39 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  cb(arr.length-1); 
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  let sum = x + y; 
+  cb(sum); 
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  let multi = x * y; 
+  cb(multi); 
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  let isPresent = false; 
+  for(let i=0;i < list.length; i++) {
+
+    if(list[i] === item) {
+      isPresent = true;
+      break;
+    }   
+  }
+    
+  cb(isPresent);
 }
 
 /* STRETCH PROBLEM */
@@ -66,4 +82,5 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  
 }
